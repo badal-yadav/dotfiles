0b9f02269d90aea0de4efd7e306c38160949d71b
badal-yadav <badalstuartbrown@gmail.com>
2023-03-25 20:58:02 +0530

    initial commit

---
+++ waybar/.config/waybar/scripts/dunst.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+COUNT=$(dunstctl count waiting)
+ENABLED=
+DISABLED=
+if [ $COUNT != 0 ]; then DISABLED=" $COUNT"; fi
+if dunstctl is-paused | grep -q "false" ; then echo $ENABLED; else echo $DISABLED; fi
